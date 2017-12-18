d38d95ed3c80398f0f581cfd827212abbdc434a5
colesayer <colesayershapiro@gmail.com>
2017-12-18 13:31:37 -0500

    added object and texture loader

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ window.addEventListener('DOMContentLoaded', () =>{
   //CAMERA
   const camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 1, 100000);
     camera.position.set(0, 0, 300)
-    // camera.up = new THREE.Vector3(0,0,0)
-    // camera.lookAt(new THREE.Vector3(0, 0, 0))
 
   controls = new THREE.OrbitControls( camera, renderer.domElement )
 
@@ -28,85 +26,30 @@ const pointLight = new THREE.PointLight(0xffffff, 0.5)
   pointLight.position.set(0, 0, 500)
   scene.add(pointLight);
 
-    //CUBE OBJECT
-// const cubeGeometry = new THREE.BoxGeometry(100, 100, 100) //maybe CubeGeometry
-// const cubeMaterial = new THREE.MeshLambertMaterial({color: "#9eb8e2"});
-// const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-//     cubeMesh.position.set(0, 250, 0)
-//     scene.add(cubeMesh)
 
-// const loader = new THREE.JSONLoader();
-// //   loader.load( "./models/sock2.json", addModelToScene );
-// //
-// //   // After loading JSON from our file, we add it to the scene
-// // function addModelToScene( geometry, materials ) {
-// //   var material = new THREE.MeshFaceMaterial(materials);
-// //   model = new THREE.Mesh( geometry, material );
-// //   model.scale.set(0.5,0.5,0.5);
-// //   scene.add( model );
-// // }
+//texture
+var sockImageURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513562483/7frj09nru4zu_lorrln.png"
+const textureLoader = new THREE.TextureLoader()
+var texture = textureLoader.load(sockImageURL)
 
-// var mesh = null;
-// function initMesh() {
-//     var loader = new THREE.JSONLoader();
-//     loader.load('./marmelab-logo.json', function(geometry) {
-//         mesh = new THREE.Mesh(geometry);
-//         scene.add(mesh);
-//     });
-// }
+var mesh
 
-var sockMesh = null;
 function initMesh(){
   console.log("in loader")
-  var loader = new THREE.JSONLoader();
-  loader.load('./models/cube2.json', function(geometry){
-    console.log("in initMesh:", geometry)
-    const material = new THREE.MeshLambertMaterial({color: 0x00ff00})
-    sockMesh = new THREE.Mesh(geometry, material);
-    scene.add(sockMesh)
-  })
-}
+  var loader = new THREE.OBJLoader();
+  loader.load('./models/cube.obj', function(object){
+    console.log("in initMesh:", object)
+    object.traverse(function(child){
+      if(child instanceof THREE.Mesh){
+        child.material.map = texture;
+      }
+    })
+    scene.add(object)
+    })
+  }
 
 initMesh()
 
-// const sockImageURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513562483/7frj09nru4zu_lorrln.png"
-//
-// const sockLoader = new THREE.TextureLoader()
-// sockLoader.load(sockImageURL, (texture) => {
-//   const sockGeometry = new THREE.JSONLoader();
-//   sockGeometry.load('./models/cube.json', function(geometry){
-//     const sockMaterial = new THREE.MeshBasicMaterial({map: texture})
-//     const sockMesh = new THREE.Mesh(sockGeometry, sockMaterial)
-//     scene.add(sockMesh)
-//   })
-// })
-
-// const sockImageURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513562483/7frj09nru4zu_lorrln.png"
-//
-//
-//   const sockGeometry = new THREE.JSONLoader();
-//   sockGeometry.load('./models/cube2.json', function(geometry){
-//     console.log("sockgeometry:", geometry)
-//     const sockMaterial = new THREE.MeshPhongMaterial({transparent: false, map: THREE.ImageUtils.loadTexture(sockImageURL)})
-//     const sockMesh = new THREE.Mesh(sockGeometry, sockMaterial)
-//     scene.add(sockMesh)
-//   })
-// //
-// const floorImageUrl = gallery.floor_texture
-//   const floorLoader = new THREE.TextureLoader();
-//     floorLoader.load(floorImageUrl, (texture) => {
-//       const floorGeometry = new THREE.PlaneGeometry( dimX, dimY)
-//       const floorMaterial = new THREE.MeshBasicMaterial({map: texture, overdraw: 0.5})
-//       const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
-//       floorMesh.position.y = 0;
-//       floorMesh.rotation.x = - Math.PI / 2;
-//       floorMesh.name="floor"
-//       // let floorBox = new THREE.BoxHelper( floorMesh );
-//       // scene.add(floorBox);
-//       galleryGroup.add(floorMesh)
-//       // addToGalleryBoxArray(floorBox)
-//       // boxGroup.add(floorBox)
-//     })
 
 
 
