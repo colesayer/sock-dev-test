540884010a86ce6327cd95026c6add7480eb623a
colesayer <colesayershapiro@gmail.com>
2017-12-19 17:09:46 -0500

    added heel, toe, and welt

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', () =>{
   const renderer = new THREE.WebGLRenderer({antialias: true})
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.shadowMapSoft = true;
   renderer.setClearColor(0xffffff, 1)
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.domElement.style.zIndex = 5;
@@ -23,69 +24,162 @@ window.addEventListener('DOMContentLoaded', () =>{
   const keyLight = new THREE.AmbientLight(0xffffff, 0.5)
     scene.add(keyLight);
 
-    //LIGHT2
-// const pointLight = new THREE.PointLight(0xffffff, 0.5)
-//   pointLight.position.set(0, 0, 100)
-//   scene.add(pointLight);
+  //Create a PointLight and turn on shadows for the light
+  var light = new THREE.PointLight( 0xffffff, 1 );
+  light.position.set( 25, 50, 10 );
+  light.castShadow = true;            // default false
+  light.shadowDarkness = 0.5;
+  scene.add( light );
 
-//Create a SpotLight and turn on shadows for the light
-// var light = new THREE.SpotLight( 0xffffff, 0.5 );
-// light.position.set(0,400,100)          // default false
-// scene.add( light );
+  //Set up shadow properties for the light
+  light.shadow.mapSize.width = 2048;  // default
+  light.shadow.mapSize.height = 2048; // default
+  light.shadow.camera.near = 0.5;       // default
+  light.shadow.camera.far = 500      // default
 
-
-//texture
-var sockImageURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513642197/trex_kids_l0rknl.bmp"
-const textureLoader = new THREE.TextureLoader()
-var texture = textureLoader.load(sockImageURL)
-texture.offset.y -= 1;
-texture.wrapS = THREE.RepeatWrapping;
-texture.repeat.set(2, 2)
-
-// var bumpMapURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513647477/knit_texture_seamless_132n_ek1fsj.jpg"
-var bumpMapURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513647868/knit_texture_seamless_132n_fine_q6pl3n.jpg"
-const bumpTextureLoader = new THREE.TextureLoader()
-var clothBumpMap = bumpTextureLoader.load(bumpMapURL)
-
-clothBumpMap.wrapT = THREE.RepeatWrapping;
-clothBumpMap.wrapS = THREE.RepeatWrapping;
-clothBumpMap.repeat.set(4, 4)
-
-var mesh
 
 var displayGroup = new THREE.Group()
 
 
-  var sockLoader1 = new THREE.OBJLoader();
-  sockLoader1.load('./models/sockfolded1.obj', function(object){
-    console.log("in initMesh:", object)
-    object.traverse(function(child){
-      if(child instanceof THREE.Mesh){
-        child.material.map = texture;
-        child.material.bumpMap = clothBumpMap;
-        child.material.bumpScale = 0.12
-          child.castShadow = true;
-      }
-    })
-    displayGroup.add(object)
-    })
+//SOCK BODY
 
+    //texture
+    var sockBodyImageURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513720856/floral_200n_copy_dhv1yy.bmp"
+    const sockBodyTextureL = new THREE.TextureLoader()
+    var sockBodyTexture = sockBodyTextureL.load(sockBodyImageURL)
+    sockBodyTexture.offset.y -= 1;
+    sockBodyTexture.wrapS = THREE.RepeatWrapping;
+    sockBodyTexture.repeat.set(2, 2)
 
-    var sockLoader2 = new THREE.OBJLoader();
-    sockLoader2.load('./models/sock3.obj', function(object){
+    //BODY MATERIAL
+    var sockBodyMaterial = new THREE.MeshPhongMaterial({color: "white"})
+
+    // var sockBodyBumpMapURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513647477/knit_texture_seamless_132n_ek1fsj.jpg"
+    var sockBodyBumpMapURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513647868/knit_texture_seamless_132n_fine_q6pl3n.jpg"
+    const sockBodyBumpTextureLoader = new THREE.TextureLoader()
+    var sockBodyBumpMap = sockBodyBumpTextureLoader.load(sockBodyBumpMapURL)
+
+    sockBodyBumpMap.wrapT = THREE.RepeatWrapping;
+    sockBodyBumpMap.wrapS = THREE.RepeatWrapping;
+    sockBodyBumpMap.repeat.set(4, 4)
+
+    var mesh
+
+    var sockBodyLoader = new THREE.OBJLoader();
+    sockBodyLoader.load('./models/exploded/body.obj', function(object){
       console.log("in initMesh:", object)
       object.traverse(function(child){
         if(child instanceof THREE.Mesh){
-          child.material.map = texture;
-          child.material.bumpMap = clothBumpMap;
+          console.log("SOCK:", child)
+          child.material = sockBodyMaterial;
+          child.material.map = sockBodyTexture;
+          child.material.bumpMap = sockBodyBumpMap;
           child.material.bumpScale = 0.12
             child.castShadow = true;
+            child.receiveShadow = true;
 
         }
       })
       object.position.set(.5, 0, 0)
       displayGroup.add(object)
       })
+
+  //SOCK TOE
+
+    //TOE MATERIAL
+    var sockToeMaterial = new THREE.MeshPhongMaterial({color: "green"})
+
+    //TOE BUMP MAP
+    var sockToeBumpMapURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513647868/knit_texture_seamless_132n_fine_q6pl3n.jpg"
+    const sockToeBumpTextureLoader = new THREE.TextureLoader()
+    var sockToeBumpMap = sockToeBumpTextureLoader.load(sockToeBumpMapURL)
+
+    sockToeBumpMap.wrapT = THREE.RepeatWrapping;
+    sockToeBumpMap.wrapS = THREE.RepeatWrapping;
+    sockToeBumpMap.repeat.set(.25, .25)
+
+      var sockToeLoader = new THREE.OBJLoader();
+      sockToeLoader.load('./models/exploded/toe.obj', function(object){
+        console.log("in initMesh:", object)
+        object.traverse(function(child){
+          if(child instanceof THREE.Mesh){
+            console.log("SOCK:", child)
+            child.material = sockToeMaterial;
+            child.material.bumpMap = sockToeBumpMap;
+            child.material.bumpScale = 0.12
+            //   child.castShadow = true;
+            //   child.receiveShadow = true;
+
+          }
+        })
+        object.position.set(.5, 0, 0)
+        displayGroup.add(object)
+        })
+
+//SOCK HEEL
+
+      //TOE MATERIAL
+      var sockHeelMaterial = new THREE.MeshPhongMaterial({color: "green"})
+
+      //TOE BUMP MAP
+      var sockHeelBumpMapURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513647868/knit_texture_seamless_132n_fine_q6pl3n.jpg"
+      const sockHeelBumpMapTextureLoader = new THREE.TextureLoader()
+      var sockHeelBumpMap = sockHeelBumpMapTextureLoader.load(sockHeelBumpMapURL)
+
+      sockHeelBumpMap.wrapT = THREE.RepeatWrapping;
+      sockHeelBumpMap.wrapS = THREE.RepeatWrapping;
+      sockHeelBumpMap.repeat.set(.5, .5)
+
+      var sockHeelLoader = new THREE.OBJLoader();
+      sockHeelLoader.load('./models/exploded/heel.obj', function(object){
+        console.log("in initMesh:", object)
+        object.traverse(function(child){
+          if(child instanceof THREE.Mesh){
+            console.log("SOCK:", child)
+            child.material = sockHeelMaterial;
+            child.material.bumpMap = sockHeelBumpMap;
+            child.material.bumpScale = 0.12
+              child.castShadow = true;
+              child.receiveShadow = true;
+
+          }
+        })
+        object.position.set(.5, 0, 0)
+        displayGroup.add(object)
+        })
+
+
+//SOCK WELT
+          //TOE MATERIAL
+          var sockWeltMaterial = new THREE.MeshPhongMaterial({color: "green"})
+
+          //TOE BUMP MAP
+          var sockWeltBumpMapURL = "http://res.cloudinary.com/dwnehv6tb/image/upload/v1513647868/knit_texture_seamless_132n_fine_q6pl3n.jpg"
+          const sockWeltBumpMapTextureLoader = new THREE.TextureLoader()
+          var sockWeltBumpMap = sockWeltBumpMapTextureLoader.load(sockWeltBumpMapURL)
+
+          sockWeltBumpMap.wrapT = THREE.RepeatWrapping;
+          sockWeltBumpMap.wrapS = THREE.RepeatWrapping;
+          sockWeltBumpMap.repeat.set(.5, .5)
+
+
+          var sockWeltLoader = new THREE.OBJLoader();
+          sockWeltLoader.load('./models/exploded/welt.obj', function(object){
+            console.log("in initMesh:", object)
+            object.traverse(function(child){
+              if(child instanceof THREE.Mesh){
+                console.log("SOCK:", child)
+                child.material = sockWeltMaterial;
+                child.material.bumpMap = sockWeltBumpMap;
+                child.material.bumpScale = 0.12
+                  child.castShadow = true;
+                  child.receiveShadow = true;
+
+              }
+            })
+            object.position.set(.5, 0, 0)
+            displayGroup.add(object)
+            })
 
   // const pedestalGeometry = new THREE.BoxGeometry(10, 5, 10)
   // const pedestalMaterial = new THREE.MeshStandardMaterial({color: "blue"})
@@ -99,41 +193,12 @@ var displayGroup = new THREE.Group()
 
 
 
-  scene.add(displayGroup)
 
-//Create a PointLight and turn on shadows for the light
-var light = new THREE.PointLight( 0xffffff, 1 );
-light.position.set( 25, 50, 10 );
-light.castShadow = true;            // default false
-scene.add( light );
 
-//Set up shadow properties for the light
-light.shadow.mapSize.width = 512;  // default
-light.shadow.mapSize.height = 512; // default
-light.shadow.camera.near = 0.5;       // default
-light.shadow.camera.far = 500      // default
 
-// //Create a sphere that cast shadows (but does not receive them)
-// var sphereGeometry = new THREE.SphereBufferGeometry( 5, 32, 32 );
-// var sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
-// var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-// sphere.castShadow = true; //default is false
-// sphere.receiveShadow = false; //default
-// scene.add( sphere );
-//
-//Create a plane that receives shadows (but does not cast them)
-var planeGeometry = new THREE.PlaneBufferGeometry( 20, 20, 32, 32 );
-var planeMaterial = new THREE.MeshStandardMaterial( { color: "white" } )
-var plane = new THREE.Mesh( planeGeometry, planeMaterial );
-plane.receiveShadow = true;
-plane.position.y = 3;
-plane.position.x = -3;
-plane.rotation.x = - Math.PI / 2;
-scene.add( plane );
 
-  var helper = new THREE.CameraHelper( light.shadow.camera );
-  scene.add( helper );
 
+scene.add(displayGroup)
 
   //RENDER LOOP
   requestAnimationFrame(render);
@@ -147,3 +212,19 @@ scene.add( plane );
 
     }
 })
+
+
+// var sockLoader1 = new THREE.OBJLoader();
+// sockLoader1.load('./models/sockfolded1.obj', function(object){
+//   console.log("in initMesh:", object)
+//   object.traverse(function(child){
+//     if(child instanceof THREE.Mesh){
+//       child.material.map = texture;
+//       child.material.bumpMap = clothBumpMap;
+//       child.material.bumpScale = 0.12
+//         child.castShadow = true;
+//         child.receiveShadow = true;
+//     }
+//   })
+//   displayGroup.add(object)
+//   })
